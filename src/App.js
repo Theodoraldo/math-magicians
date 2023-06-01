def3f7eb49f733ec4a16d65df6372296b223fbbc
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-06-01 12:14:02 +0000

    Render fetch API UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import Data from './components/Data';
 
 function App() {
   return (
     <div className="App">
+      <Data />
       <Calculator />
     </div>
   );
