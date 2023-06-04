8e4cf32fa9ff4a000991f29dd5cdd197fb4b9bde
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-06-04 12:07:31 +0000

    Modify router to app.js file and few css styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Data from './components/Data';
+import Home from './components/Home';
+import './App.css';
 
 function App() {
   return (
     <>
-      <h1 className="app-name">Math Magicians</h1>
-      <div className="App">
-        <Data />
-        <Calculator />
+      <div>
+        <h1 className="app-name">Math Magicians</h1>
       </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='quote' element={<Data />} />
+        <Route path='calculator' element={<Calculator />} />
+      </Routes>
     </>
   );
 }
