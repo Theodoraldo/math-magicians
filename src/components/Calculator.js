34406ff1d9c269ba97369a78c07818a6f6cee962
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-06-04 15:26:45 +0000

    Modify Calculator component files

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -30,28 +30,31 @@ const Calculator = () => {
   }
 
   return (
-    <div className="container">
-      <div className="output">{result || 0}</div>
-      <div className="buttons">
-        <button className="btn-links" type="button" value="AC" onClick={arithmetic}>AC</button>
-        <button className="btn-links" type="button" value="+/-" onClick={arithmetic}>+/-</button>
-        <button className="btn-links" type="button" value="%" onClick={arithmetic}>%</button>
-        <button className="btn-links operator" type="button" value="÷" onClick={arithmetic}>÷</button>
-        <button className="btn-links" type="button" value="7" onClick={arithmetic}>7</button>
-        <button className="btn-links" type="button" value="8" onClick={arithmetic}>8</button>
-        <button className="btn-links" type="button" value="9" onClick={arithmetic}>9</button>
-        <button className="btn-links operator" type="button" value="x" onClick={arithmetic}>x</button>
-        <button className="btn-links" type="button" value="4" onClick={arithmetic}>4</button>
-        <button className="btn-links" type="button" value="5" onClick={arithmetic}>5</button>
-        <button className="btn-links" type="button" value="6" onClick={arithmetic}>6</button>
-        <button className="btn-links operator" type="button" value="-" onClick={arithmetic}>-</button>
-        <button className="btn-links" type="button" value="1" onClick={arithmetic}>1</button>
-        <button className="btn-links" type="button" value="2" onClick={arithmetic}>2</button>
-        <button className="btn-links" type="button" value="3" onClick={arithmetic}>3</button>
-        <button className="btn-links operator" type="button" value="+" onClick={arithmetic}>+</button>
-        <button className="btn-links span-2" type="button" value="0" onClick={arithmetic}>0</button>
-        <button className="btn-links" type="button" value="." onClick={arithmetic}>.</button>
-        <button className="btn-links operator" type="button" value="=" onClick={arithmetic}>=</button>
+    <div className="App">
+      <div className="header">Let&#39;s do some Maths</div>
+      <div className="container">
+        <div className="output">{result || 0}</div>
+        <div className="buttons">
+          <button className="btn-links" type="button" value="AC" onClick={arithmetic}>AC</button>
+          <button className="btn-links" type="button" value="+/-" onClick={arithmetic}>+/-</button>
+          <button className="btn-links" type="button" value="%" onClick={arithmetic}>%</button>
+          <button className="btn-links operator" type="button" value="÷" onClick={arithmetic}>÷</button>
+          <button className="btn-links" type="button" value="7" onClick={arithmetic}>7</button>
+          <button className="btn-links" type="button" value="8" onClick={arithmetic}>8</button>
+          <button className="btn-links" type="button" value="9" onClick={arithmetic}>9</button>
+          <button className="btn-links operator" type="button" value="x" onClick={arithmetic}>x</button>
+          <button className="btn-links" type="button" value="4" onClick={arithmetic}>4</button>
+          <button className="btn-links" type="button" value="5" onClick={arithmetic}>5</button>
+          <button className="btn-links" type="button" value="6" onClick={arithmetic}>6</button>
+          <button className="btn-links operator" type="button" value="-" onClick={arithmetic}>-</button>
+          <button className="btn-links" type="button" value="1" onClick={arithmetic}>1</button>
+          <button className="btn-links" type="button" value="2" onClick={arithmetic}>2</button>
+          <button className="btn-links" type="button" value="3" onClick={arithmetic}>3</button>
+          <button className="btn-links operator" type="button" value="+" onClick={arithmetic}>+</button>
+          <button className="btn-links span-2" type="button" value="0" onClick={arithmetic}>0</button>
+          <button className="btn-links" type="button" value="." onClick={arithmetic}>.</button>
+          <button className="btn-links operator" type="button" value="=" onClick={arithmetic}>=</button>
+        </div>
       </div>
     </div>
   );
